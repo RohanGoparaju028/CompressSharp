e20c7a672eb7443488ab11b995fc1587de5e0cc8
Rohan Goparaju <goparajurohan41@gmail.com>
2026-06-23 17:22:21 -0400

    Authentication for AWS

---
+++ Authenticate.cs
@@ -0,0 +1,37 @@
+using Amazon;
+using Amazon.S3;
+using Amazon.Runtime;
+using dotenv.net;
+using Amazon.SecurityToken;
+using Amazon.SecurityToken.Model;
+namespace CompressSharp.Auth;
+
+public class Authenticate
+{
+    public async Task<bool> Verify()
+    {
+        try
+        {
+            DotEnv.Load(options: new DotEnvOptions(envFilePaths: new[]{".env"}));
+            var envVar = DotEnv.Read();
+            var accessKeyId = envVar["ACCESS_KEY_ID"];
+            var secret_acess_key_id = envVar["SECRET_ACCESS_KEY"];
+            var credential = new BasicAWSCredentials(accessKeyId, secret_acess_key_id);
+            var region = envVar["REGION"];
+            var regionEndpoint = RegionEndpoint.GetBySystemName(region);
+            using var stdClient = new AmazonSecurityTokenServiceClient(credential, regionEndpoint);
+            var _ = await stdClient.GetCallerIdentityAsync(new GetCallerIdentityRequest());
+            return true;
+        }
+        catch (AmazonSecurityTokenServiceException ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+            return false;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+            return false;
+        }
+    }
+}
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+### CompressSharp 
+CompressSharp is a compression library that compresses large dataset stored in S3.The library is primarily writtern in C#.
+The library uses the individiual AWS credential and works with the dataset you are accessed to.
+The library has 3 functinalites 
+1) Authenticate and Aurtorize the users credentials
+2) Find the dataset that you wanna compress
+3) Compress and install the dataset in the current directory from which the library is being used.
